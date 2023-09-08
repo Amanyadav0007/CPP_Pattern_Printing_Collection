19d73ad130ec2009114f64f30f9f8e647a87a5c2
@curious_coder_aman <113257634+Amanyadav0007@users.noreply.github.com>
2023-09-09 00:57:01 +0530

    Add files via upload

---
+++ 4-NumericHalfPyramid.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col <= row; col++)
+        {
+            cout << col + 1;
+        }
+        cout << endl;
+    }
+    
+
+    return 0;
+}
